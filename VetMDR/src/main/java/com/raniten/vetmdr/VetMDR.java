bf2d0ae410f8956ff538174faf7b807291e812f3
Marcelo Ranzani <raniten@gmail.com>
2023-11-22 17:08:32 -0300

    Inicio del proyecto

---
+++ VetMDR/src/main/java/com/raniten/vetmdr/VetMDR.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.raniten.vetmdr;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class VetMDR {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
